6669fa62770fa2fa88227a399dfcb44813c7fc22
Remo <Remo@REMO-PC>
2016-05-15 15:00:41 +0200

    Stored Procedure GetCustomerOrders from Chapter 13, page 504 added

---
+++ tsql2012/Sales/GetCustomerOrders.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE Sales.GetCustomerOrders
+    @custid AS INT ,
+    @orderdatefrom AS DATETIME = '19000101' ,
+    @orderdateto AS DATETIME = '99991231' ,
+    @numrows AS INT = 0 OUTPUT
+AS
+    BEGIN
+        SET NOCOUNT ON;
+
+        SELECT  o.orderid, o.custid, o.shipperid, o.orderdate, o.requireddate, o.shippeddate		
+        FROM    Sales.Orders AS o
+        WHERE  o.custid = COALESCE(@custid, 'unknown')
+                AND o.orderdate >= @orderdatefrom
+                AND o.orderdate < @orderdateto;
+        SET @numrows = @@ROWCOUNT;
+        RETURN;
+    END;	
