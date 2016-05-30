2045d2c19b9ce1dd420a2e905567b4cf6646620a
Remo <Remo@REMO-PC>
2016-05-30 22:35:21 +0200

    chapter 3 is done! practise answers

---
+++ tsql2012/end of chapter 3 practise questions.sql
@@ -0,0 +1,11 @@
+﻿from where group by having top select order by
+from where group by having offset fetch select order by
+
+skipping rows/paging
+default deterministic query
+
+what is needed for deterministic: include all rows that might have duplicate entries
+in the order by list
+
+with top: with ties and by using order by with all columns with duplicates
+with offset-fetch the same
